f9584945da28279de071dd11c0a4155d40d3a8af
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-12-17 13:57:45 +0200

    new commit

---
+++ features/annotations/functions.ts
@@ -0,0 +1,19 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+};
