d75a87f81d7e1aafb27c3d9283f4732c8517682f
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-12-17 09:33:12 +0200

    understanding type annotations with variables

---
+++ features/annotations/variables.ts
@@ -0,0 +1,11 @@
+const apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+
+// built in objects
+let now: Date = new Date();
+
+// Array
+let colors: string[] = ['red', 'green', 'blue'];
