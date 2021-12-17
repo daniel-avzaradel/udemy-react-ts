f61c7dc15f97188df35bc8caae55c3befcec1b8d
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-12-17 22:12:01 +0200

    understanding interfaces in TS

---
+++ features/tuples.ts
@@ -0,0 +1,15 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+};
+
+const pepsi: [string, boolean, number] = ['brown', true, 40];
+// a tuple has a specific order of properties that cannot be exchanged
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
