3ae6b67a08cf12127c066219d112fef37e9fd5e8
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-12-17 20:50:14 +0200

    multiple types in arrays with typescript

--- features/arrays.ts
+++ features/arrays.ts
@@ -1,1 +1,16 @@
 const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMaker: string[][] = [];
+
+// Help with inference when extracting values
+const myCar = carMakers[0];
+const myCar2 = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push('porsche');
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
