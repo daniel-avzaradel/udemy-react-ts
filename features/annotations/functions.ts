cd0b8f8087cf46e10c05a6dff76fddc602be7515
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-12-17 14:45:11 +0200

    arrays in typescript

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -17,3 +17,27 @@ function multiply(a: number, b: number): number {
 const logger = (message: string): void => {
   console.log(message);
 };
+
+const throwError = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(forecast);
