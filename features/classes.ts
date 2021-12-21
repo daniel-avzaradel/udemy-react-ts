03ced67a8908a5b80fd06e4651729f2ec9b3d12a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-12-21 18:57:20 +0200

    classes in typescript

---
+++ features/classes.ts
@@ -0,0 +1,12 @@
+class Vehicle {
+  drive(): void {
+    console.log('test test');
+  }
+  honk(): void {
+    console.log('beep');
+  }
+}
+
+const vehicle = new Vehicle();
+vehicle.drive();
+vehicle.honk();
