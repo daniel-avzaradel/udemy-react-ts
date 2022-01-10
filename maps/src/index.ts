21b6e4fdf7d9215c54d07718b29901b98bac7bcc
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-01-10 10:25:32 +0200

    adding google maps package

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there, console');
+/// <reference types="@types/google.maps" />
+
+import { Company } from './Company';
+import { User } from './User';
+
+const user = new User();
+const company = new Company();
+
+console.log('user is: ', user);
+console.log('company is: ', company);
