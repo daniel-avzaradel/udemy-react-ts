ec7d77246fc402a2cbbffd7ef6f9a97a7a3b7d2a
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-01-12 23:06:45 +0200

    adding markers to googlemaps

--- maps/dist/index.js
+++ maps/dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62914" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60541" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
