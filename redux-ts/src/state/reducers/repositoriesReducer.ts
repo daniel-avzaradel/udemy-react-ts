58a70293f94226a9f4f926896c39822784572bf6
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2022-01-21 06:35:39 +0200

    applying Action Interfaces

--- redux-ts/src/state/reducers/repositoriesReducer.ts
+++ redux-ts/src/state/reducers/repositoriesReducer.ts
@@ -4,11 +4,32 @@ interface RepositoryState {
   data: string[];
 }
 
-const reducer = (state: RepositoryState, action: any): RepositoryState => {
+interface SearchRepositoriesAction {
+  type: 'search_repositories';
+}
+
+interface SearchRepositoriesSuccessAction {
+  type: 'search_repositories_success';
+  payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+  type: 'search_repositories_error';
+  payload: string;
+}
+
+const reducer = (
+  state: RepositoryState,
+  action:
+    | SearchRepositoriesAction
+    | SearchRepositoriesSuccessAction
+    | SearchRepositoriesErrorAction
+): RepositoryState => {
   switch (action.type) {
     case 'search_repositories':
       return { loading: true, error: null, data: [] };
     case 'search_repositories_success':
+      // 100% certainty that 'action' satisfies the SearchRepositoriesSuccessAction state
       return { loading: false, error: null, data: action.payload };
     case 'search_repositories_error':
       return { loading: false, error: action.payload, data: [] };
